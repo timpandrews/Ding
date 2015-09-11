b892e995d74cc38a4017e97605795590330186e8
Tim Andrews <tim.andrews@imagineifinc.com>
2015-09-11 05:08:54 -0400

    Original Xcode Project, Icons, Landing Screen

---
+++ Ding/Ding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ding
+//
+//  Created by Tim.Andrews on 9/11/15.
+//  Copyright (c) 2015 justTim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
