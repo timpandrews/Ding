99a5cb7b1796da4b274ca95cb47bc71e34b4b461
Tim Andrews <tim.andrews@imagineifinc.com>
2015-09-11 05:19:12 -0400

    Results View Controller & Segue

---
+++ Ding/Ding/ResultsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ResultsViewController.swift
+//  Ding
+//
+//  Created by Tim.Andrews on 9/11/15.
+//  Copyright (c) 2015 justTim. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var lblResults: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        lblResults.text = "Results go here!"
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
